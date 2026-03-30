b4c2578cde81c6b90416032a4af17ab440fb6aa6
Mayank Ahirwar <ahirwarmayank100@gmail.com>
2026-03-30 23:09:59 +0530

    Time: 2 ms (30.44%) | Memory: 43.4 MB (21.76%) - LeetSync

--- String/520-detect-capital/detect-capital.java
+++ String/520-detect-capital/detect-capital.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(word.equals(word.toUpperCase())){
-            return true;
-        }
-        if(word.equals(word.toLowerCase())){
-            return true;
-        }
-        if(word.charAt(0) == Character.toUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase())){
-            return true;
-        }
-        return false;
+        return word.equals(word.toUpperCase()) || word.equals(word.toLowerCase()) || (Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase()));
     }
 }
