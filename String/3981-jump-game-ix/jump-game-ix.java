96be880579a1d2bfad4f75af40ff6e7d90f40f66
Mayank Ahirwar <ahirwarmayank100@gmail.com>
2026-05-07 20:28:01 +0530

    Time: 5 ms (87.10%) | Memory: 198.7 MB (61.29%) - LeetSync

--- String/3981-jump-game-ix/jump-game-ix.java
+++ String/3981-jump-game-ix/jump-game-ix.java
@@ -2,31 +2,26 @@ class Solution {
     public int[] maxValue(int[] nums) {
         int n = nums.length;
 
-        int[] pre = new int[n];
-        int[] suf = new int[n];
-        int[] res = new int[n];
+        int[] ans= new int[n];
+        int[] preMax = new int[n];
 
-       pre[0] = nums[0];
+        preMax[0] = nums[0];
+
         for (int i = 1; i < n; i++) {
-            pre[i] = Math.max(pre[i - 1], nums[i]);
+            preMax[i] = Math.max(preMax[i - 1], nums[i]);
         }
 
-        suf[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suf[i] = Math.min(suf[i + 1], nums[i]);
+        int sufMin = Integer.MAX_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (preMax[i] > sufMin)
+                ans[i] = ans[i + 1];
+            else
+                ans[i] = preMax[i];
+
+            sufMin = Math.min(sufMin, nums[i]);
         }
 
-        res[n - 1] = pre[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-
-            if (pre[i] > suf[i + 1]) {
-                res[i] = res[i + 1];
-            }else {
-                res[i] = pre[i];
-            }
-        }
-
-        return res;
+        return ans;
     }
 }
